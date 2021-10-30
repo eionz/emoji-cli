9b9b847a0c772b814e24e1c3cd99f6d4f217a718
Rick Li <rick@onerise.tech>
2021-10-31 01:10:29 +0800

    bugfix: keybinding did'nt work at fish 3.3.1

--- key_bindings.fish
+++ key_bindings.fish
@@ -2,6 +2,6 @@ set -q EMOJI_CLI_KEYBIND; or set -l EMOJI_CLI_KEYBIND \cs
 
 bind $EMOJI_CLI_KEYBIND "__emoji-cli"
 
-if bind -M insert >/dev/null ^/dev/null
+if bind -M insert >/dev/null ^ /dev/null
   bind -M insert $EMOJI_CLI_KEYBIND "__emoji-cli"
 end
